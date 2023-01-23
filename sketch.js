8bdc0054c6450de64f3435da2901beab7dfadec0
Derek Lorenzo <87837145+DerekLorenzo@users.noreply.github.com>
2023-01-23 15:21:46 -0500

    Update sketch.js
    added arcs to represent h, m, s

--- sketch.js
+++ sketch.js
@@ -1,15 +1,37 @@
 function setup() {
-	createCanvas(800,600); // make an HTML canvas element width x height pixels
+	createCanvas(800,400); // make an HTML canvas element width x height pixels
 }
 
 function draw() {
 	background(225);
-	textSize(100);
-	fill(180);
-	text(hour(), 10, 30);
-	text("sup");
-	fill(100);
-	text(minute(), 10, 60);
-	fill(0);
-	text(second(), 10, 90);
+    
+    //Hour
+    let h = hour();
+    let hourAngle = map(h,0,23,180,360);
+    push();
+    noFill();
+    stroke(255,0,0);
+    strokeWeight(32);
+    arc(400,400,680,680,radians(180),radians(hourAngle));
+    pop();
+  
+    //Minute
+    let m = minute();
+    let minuteAngle = map(m,0,59,180,360); 
+    push();
+    noFill();
+    stroke(255,0,255);
+    strokeWeight(32);
+    arc(400,400,600,600,radians(180),radians(minuteAngle));
+    pop();
+  
+    //Second
+    let s = second();
+    let secondAngle = map(s,0,59,180,360);
+    push();
+    noFill();
+    stroke(0,0,255);
+    strokeWeight(32);
+    arc(400,400,520,520,radians(180),radians(secondAngle));
+    pop();
 }
