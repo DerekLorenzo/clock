d2f4fb73a8bd5bade30a973c0c2c9be4e71d0566
Derek Lorenzo <87837145+DerekLorenzo@users.noreply.github.com>
2023-01-23 20:13:22 -0500

    Update sketch.js
    fixed minor math issues and cleaned up design

--- sketch.js
+++ sketch.js
@@ -1,40 +1,40 @@
 function setup() {
-	createCanvas(800,400); // make an HTML canvas element width x height pixels
+    createCanvas(800,400); // make an HTML canvas element width x height pixels
 }
 
 function drawTicks()
 {
-  //Outer Ticks
-  let center = createVector(width/2,height);
-  let midnight = createVector(-1,0);
-  let innerRadius = 370;
-  let outerRadius = 375;
-  
-  //draw a tick at each hour 
-  for(let i = 0 ; i<25 ; i++)
-  {
-    let b = createVector(cos(PI*i/24),sin(PI*i/24));
-    let direction = createVector(midnight.x*b.x-midnight.y*b.y,midnight.y*b.x+midnight.x*b.y);
-    line(center.x+direction.x*innerRadius,
-         center.y+direction.y*innerRadius,
-         center.x+direction.x*outerRadius,
-         center.y+direction.y*outerRadius);
-  }
-  
-  //Inner Ticks
-  let secondInnerRadius = 225;
-  let secondOuterRadius = 230;
-  
-  //draw a tick at each minute/second
-  for(let i = 0 ; i<61 ; i++)
-  {
-    let b = createVector(cos(PI*i/60),sin(PI*i/60));
-    let direction = createVector(midnight.x*b.x-midnight.y*b.y,midnight.y*b.x+midnight.x*b.y);
-    line(center.x+direction.x*secondInnerRadius,
-         center.y+direction.y*secondInnerRadius,
-         center.x+direction.x*secondOuterRadius,
-         center.y+direction.y*secondOuterRadius);
-  }
+    //Outer Ticks
+    let center = createVector(width/2,height);
+    let midnight = createVector(-1,0);
+    let innerRadius = 370;
+    let outerRadius = 375;
+
+    //draw a tick at each hour 
+    for(let i = 0 ; i<25 ; i++)
+    {
+      let b = createVector(cos(PI*i/24),sin(PI*i/24));
+      let direction = createVector(midnight.x*b.x-midnight.y*b.y,midnight.y*b.x+midnight.x*b.y);
+      line(center.x+direction.x*innerRadius,
+           center.y+direction.y*innerRadius,
+           center.x+direction.x*outerRadius,
+           center.y+direction.y*outerRadius);
+    }
+
+    //Inner Ticks
+    let secondInnerRadius = 225;
+    let secondOuterRadius = 230;
+
+    //draw a tick at each minute/second
+    for(let i = 0 ; i<61 ; i++)
+    {
+      let b = createVector(cos(PI*i/60),sin(PI*i/60));
+      let direction = createVector(midnight.x*b.x-midnight.y*b.y,midnight.y*b.x+midnight.x*b.y);
+      line(center.x+direction.x*secondInnerRadius,
+           center.y+direction.y*secondInnerRadius,
+           center.x+direction.x*secondOuterRadius,
+           center.y+direction.y*secondOuterRadius);
+    }
 }
 
 function draw() {
@@ -42,7 +42,7 @@ function draw() {
     let m = minute();
     let s = second();
     let total = 24 * 60 * 60 / 2;
-    let current = Math.abs(total - h*60*60 + m*60 + s);
+    let current = Math.abs(total - (h*60*60+m*60+s));
   
     //calculate background color
     let r = map(current, 0, total, 255, 0);
@@ -83,5 +83,11 @@ function draw() {
     noFill();
     strokeWeight(1);
     arc(400,400,460,460,radians(180),radians(360));
+    strokeWeight(2);
     drawTicks();
+  
+    //Add border
+    strokeWeight(5);
+    noFill();
+    rect(0,0,800,400);
 }
